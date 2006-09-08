39382bd51c1d983e0daef9dc1f558b4ddc73f253
mvl <mvl>
2006-09-08 08:54:06 +0000

    Bumped the VERSION string to match the actual version.

--- sources/interface/version.h
+++ sources/interface/version.h
@@ -1,6 +1,6 @@
 /*
   This is version.h
-  
+
   Copyright (C) 2004,2005 Fokko du Cloux
   part of the Atlas of Reductive Lie Groups
 
@@ -15,7 +15,7 @@ namespace atlas {
 namespace version {
 
   const char* const NAME = "the Atlas of Reductive Lie Groups Software Package";
-  const char* const VERSION = "0.2.4";
+  const char* const VERSION = "0.2.5";
 
 }
 
