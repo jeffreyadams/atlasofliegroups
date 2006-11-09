03540cbcc068205f50f5ac3b7c460b5bee050b1c
mvl <mvl>
2006-11-09 10:07:00 +0000

    Added and answer to a question in the comments

--- sources/gkmod/bruhat.h
+++ sources/gkmod/bruhat.h
@@ -5,9 +5,9 @@
 
 /*
   This is bruhat.h
-  
+
   Copyright (C) 2004,2005 Fokko du Cloux
-  part of the Atlas of Reductive Lie Groups  
+  part of the Atlas of Reductive Lie Groups
 
   See file main.cpp for full copyright notice
 */
@@ -31,13 +31,19 @@ namespace bruhat {
 
   /*!
 \brief Intended to represent the Bruhat order on K orbits on G/B, or
-on a block of representations.  
+on a block of representations.
 
 [Not seriously used in the present code; I'm not sure whether it is
 instantiated.  The classes KGB and Block have manipulators fillBruhat
-that would create BruhatOrder classes, but they seem not to be called. 
-DV 7/21/06] 
-  */ 
+that would create BruhatOrder classes, but they seem not to be called.
+DV 7/21/06]
+
+[In fact the kgb::KGB and blocks::Block classes do contain members of type
+(pointer to) BruhatOrder, and methods fillBruhat that will create instances of
+BruhatOrder accessible from those classes, but these methods are never called.
+MvL 8 Nov 2006]
+
+  */
   class BruhatOrder {
 
  private:
@@ -53,7 +59,7 @@ Entry \#j lists the numbers of the immediate predecessors of element
 
 It is assumed that element \#i can precede element \#j in the poset
 only if i < j, so the symmetrized relation determines the relation.
-  */  
+  */
   poset::SymmetricPoset d_poset;
 
  public:
