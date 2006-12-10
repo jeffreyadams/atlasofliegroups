cb03a4496bf6eafd912e2d69a7cc5520871193ba
mvl <mvl>
2006-12-10 09:42:30 +0000

    Changed comments in polynomials.h to match modular polynmial implementation

--- sources/utilities/polynomials.h
+++ sources/utilities/polynomials.h
@@ -6,7 +6,7 @@
   This is polynomials.h
 
   Copyright (C) 2004,2005 Fokko du Cloux
-  part of the Atlas of Reductive Lie Groups 
+  part of the Atlas of Reductive Lie Groups
 
   See file main.cpp for full copyright notice
 */
@@ -52,11 +52,11 @@ void safeSubtract(C&, C);
 namespace polynomials {
 
   /*!  \brief Polynomials with coefficients in C, which must be a
-       standard unsigned type.
+       modular integral type
 
-       The coefficient type C must support addition, multiplication,
-       subtraction, and std::numeric_limits<C> (used to test for
-       overflow in the safeAdd operation).
+       The coefficient type C must support addition (+,+=),
+       multiplication (*=) and subtraction (unary and binary -),
+       and comparison (<, ==, !=) (where < need not have mathematical sense)
   */
 template<typename C> class Polynomial {
 
@@ -79,7 +79,7 @@ template<typename C> class Polynomial {
   }
 
 // accessors
-  C operator[] (Degree j) const {     
+  C operator[] (Degree j) const {
     return d_data[j];
   }
 
@@ -90,12 +90,12 @@ template<typename C> class Polynomial {
   /*!
 \brief Operator < is the default from the standard library < on vector.
 
-The ordering in the boolean compare(P,Q) is a much more useful and
+  The ordering in the boolean compare(P,Q) is a much more useful and
   natural one. According to Fokko, the reason for using this unnatural
   comparison operator is probably that profiling suggested that the
   compare function was using a lot of time in Kazhdan-Lusztig
   computations.  (Each new KL polynomial must be inserted into the set
-  of 
+  of all polynomials, using a binary search like procedure, which uses '<')
   */
 bool operator< (const Polynomial& q) const { return d_data < q.d_data;
   }
