387885e6ac020231d56ba08d8a5de23c421922e6
mvl <mvl>
2007-02-19 12:53:07 +0000

    Added the module filekl, which serves for reading back a W-graph from information about a block and KL polynomials contained in three binary files

---
+++ sources/utilities/filekl.cpp
@@ -0,0 +1,415 @@
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <stdexcept>
+
+#include "filekl.h"
+#include "bits.h"
+#include "wgraph.h"
+
+namespace atlas {
+  namespace filekl {
+
+
+    // Constants and auxiliary functions
+
+const BlockElt UndefBlock= ~BlockElt(0);
+const BlockElt noGoodAscent= UndefBlock-1;
+
+const std::ios_base::openmode binary_in=
+			    std::ios_base::in
+			  | std::ios_base::binary;
+
+const std::ios_base::openmode binary_in_out=
+			    std::ios_base::in
+			  | std::ios_base::out
+			  | std::ios_base::binary;
+
+const unsigned int magic_code=0x06ABdCF0; // indication of new matrix format
+
+template <unsigned int n>
+inline ullong read_bytes(std::istream& in)
+{
+  return static_cast<unsigned char>(in.get())+(read_bytes<n-1>(in)<<8);
+}
+
+template<>
+inline ullong read_bytes<1>(std::istream& in)
+{
+  return static_cast<unsigned char>(in.get());
+}
+
+ullong read_var_bytes(unsigned int n,std::istream& in)
+{ switch(n)
+  { case 1: return read_bytes<1>(in);
+    case 2: return read_bytes<2>(in);
+    case 3: return read_bytes<3>(in);
+    case 4: return read_bytes<4>(in);
+    case 5: return read_bytes<5>(in);
+    case 6: return read_bytes<6>(in);
+    case 7: return read_bytes<7>(in);
+    case 8: return read_bytes<8>(in);
+    default: throw std::runtime_error("Illegal read_var_bytes");
+  }
+}
+
+
+    // Methods of the |polynomial_info| class
+
+polynomial_info::polynomial_info (std::ifstream& coefficient_file)
+: file(coefficient_file), n_pols(read_bytes<4>(file))
+, coef_size(), index_begin(file.tellg()), coefficients_begin()
+{ file.seekg(10,std::ios_base::cur); // skip initial 2 indices
+  coef_size=read_bytes<5>(file); // size of the |One|
+  file.seekg(index_begin+5*n_pols,std::ios_base::beg);
+  n_coef=read_bytes<5>(file)/coef_size;
+  coefficients_begin=file.tellg();
+}
+
+size_t polynomial_info::degree(KLIndex i) const
+{ if (i<2) return i-1; // quit exit for Zero and One
+  file.seekg(index_begin+5*i,std::ios_base::beg);
+  ullong index=read_bytes<5>(file);
+  ullong next_index=read_bytes<5>(file);
+  size_t length=(next_index-index)/coef_size;
+  return length-1;
+}
+
+std::vector<size_t> polynomial_info::coefficients(KLIndex i) const
+{ file.seekg(index_begin+5*i,std::ios_base::beg);
+  ullong index=read_bytes<5>(file);
+  ullong next_index=read_bytes<5>(file);
+  size_t length=(next_index-index)/coef_size;
+
+  std::vector<size_t> result(length);
+  file.seekg(coefficients_begin+index,std::ios_base::beg);
+
+  for (size_t i=0; i<length; ++i)
+    result[i]=read_var_bytes(coef_size,file);
+
+  return result;
+}
+
+// the leading coefficient is the last one stored before the next polynomial
+size_t polynomial_info::leading_coeff(KLIndex i) const
+{ if (i<2) return i;
+  file.seekg(index_begin+5*(i+1),std::ios_base::beg);
+  ullong next_index=read_bytes<5>(file);
+  file.seekg(coefficients_begin+next_index-coef_size,std::ios_base::beg);
+  return read_var_bytes(coef_size,file);
+}
+
+
+    // Methods of the |block_info class|
+
+
+BlockElt block_info::primitivize(BlockElt x, BlockElt y) const
+{
+  RankFlags d=descent_set[y];
+start:
+  if (x>=y) return x; // possibly with |x==UndefBlock|
+  const ascent_vector& ax=ascents[x];
+  for (size_t s=0; s<rank; ++s)
+    if (d[s] and ax[s]!=noGoodAscent)
+    { x=ax[s]; goto start; } // this should raise x, now try another step
+  return x; // no raising possible, stop here
+}
+
+bool  block_info::is_primitive(BlockElt x, const RankFlags d) const
+{
+  const ascent_vector& ax=ascents[x];
+  for (size_t s=0; s<ax.size(); ++s)
+    if (d[s] and ax[s]!=noGoodAscent) return false;
+  return true;
+  // now |d[s]| implies |ascents[s]==noGoodAscent| for all simple roots |s|
+}
+
+const prim_list& block_info::prims_for_descents_of(BlockElt y)
+{ RankFlags d=descent_set[y];
+  unsigned long s=d.to_ulong();
+  prim_list& result=primitives_list[s];
+  if (result.empty())
+  { for (BlockElt x=0; x<size; ++x)
+      if (is_primitive(x,d)) result.push_back(x);
+    prim_list(result).swap(result); // reallocate to fit snugly
+  }
+  return result;
+}
+
+block_info::block_info(std::ifstream& in)
+  : rank(), size(), max_length(), start_length()
+  , descent_set(), ascents(), primitives_list() // don't initialize yet
+{
+  in.seekg(0,std::ios_base::beg); // ensure we are reading from the start
+  size=read_bytes<4>(in);
+  rank=read_bytes<1>(in);
+  max_length=read_bytes<1>(in);
+
+  // read intervals of block elements for each length
+  start_length.resize(max_length+2);
+  start_length[0]=BlockElt(0);
+  for (size_t i=1; i<=max_length; ++i) start_length[i]=read_bytes<4>(in);
+  start_length[max_length+1]=size;
+
+  // read descent sets
+  descent_set.reserve(size);
+  for (BlockElt y=0; y<size; ++y)
+    descent_set.push_back(RankFlags(read_bytes<4>(in)));
+
+  // read ascent table
+  ascents.reserve(size);
+  for (BlockElt x=0; x<size; ++x)
+    {
+      ascents.push_back(ascent_vector());
+      ascent_vector& a=ascents.back();
+      a.reserve(rank);
+      for (size_t s=0; s<rank; ++s)
+	a.push_back(read_bytes<4>(in));
+    }
+
+  primitives_list.resize(1ul<<rank); // create $2^{rank}$ empty vectors
+}
+
+
+
+    // Methods of the |matrix_info| class
+
+size_t matrix_info::length (BlockElt y) const
+{ return
+    std::upper_bound(block.start_length.begin(),block.start_length.end(),y)
+    -block.start_length.begin() // index of first element of length |l(y)+1|
+    -1; // now we have just |l(y)|
+}
+
+void matrix_info::set_y(BlockElt y)
+{
+  if (y==cur_y) { matrix_file.seekg(cur_row_entries); return; }
+  cur_y=y;
+  const prim_list& weak_prims = block.prims_for_descents_of(y);
+  cur_strong_prims.resize(0);
+  // restart building from scratch, but don't deallocate storage
+
+  matrix_file.seekg(row_pos[y]);
+  size_t n_prim=read_bytes<4>(matrix_file);
+
+
+  for (size_t i=0; i<n_prim; i+=32)
+    {
+      unsigned int chunk=read_bytes<4>(matrix_file);
+      for (size_t j=0; chunk!=0; ++j,chunk>>=1) // and certainly |j<32|
+        if ((chunk&1)!=0) cur_strong_prims.push_back(weak_prims[i+j]);
+    }
+
+  {
+#ifndef NDEBUG
+    const BlockElt* i= // point after first block element of length of |y|
+      std::upper_bound(&block.start_length[0]
+  		    ,&block.start_length[block.max_length+1]
+  		    ,y);
+    const BlockElt* first= // point to first of |weak_prims| of that length
+      std::lower_bound(&weak_prims[0],&weak_prims[weak_prims.size()],*(i-1));
+    assert(cur_strong_prims.back()==*first);
+#endif
+    cur_strong_prims.back()=y; // replace by |y|
+  }
+
+  cur_row_entries=matrix_file.tellg();
+}
+
+KLIndex matrix_info::find_pol_nr(BlockElt x,BlockElt y,BlockElt& xx)
+{
+  set_y(y);
+  xx=block.primitivize(x,y);
+  if (xx>=y) return KLIndex(xx==y ? 1 : 0); // primitivisation copped out
+  strong_prim_list::const_iterator it=
+    std::lower_bound(cur_strong_prims.begin(),cur_strong_prims.end(),xx);
+  if (it==cur_strong_prims.end() or *it!=xx) return KLIndex(0); // not strong
+
+  matrix_file.seekg(4*size_t(it-cur_strong_prims.begin()),std::ios_base::cur);
+  return KLIndex(read_bytes<4>(matrix_file));
+}
+
+BlockElt matrix_info::prim_nr(unsigned int i,BlockElt y)
+{ const prim_list& weak_prims = block.prims_for_descents_of(y);
+  const BlockElt* it= // point after first block element of length of |y|
+    std::upper_bound(&block.start_length[0]
+		    ,&block.start_length[block.max_length+1]
+		    ,y);
+  const BlockElt* first= // point to first of |weak_prims| of that length
+    std::lower_bound(&weak_prims[0],&weak_prims[weak_prims.size()],*(it-1));
+  size_t limit=first-&weak_prims[0]; // limiting value for |i|
+  if (i<limit) return weak_prims[i];
+  else if (i==limit) return y;
+  std::cerr << "Limit is " << limit << ".\n";
+  throw std::runtime_error("Weakly primitive element index too large");
+}
+
+matrix_info::matrix_info
+  (std::ifstream& block_file,std::ifstream& m_file)
+: matrix_file(m_file) // store reference to the matrix file
+  , block(block_file) // read in block information
+  , row_pos(block.size) // dimension these vectors
+  , cur_y(UndefBlock), cur_strong_prims(), cur_row_entries(0)
+{
+  matrix_file.seekg(0,std::ios_base::beg);
+  if (read_bytes<4>(matrix_file)==magic_code)
+  { matrix_file.seekg(-4*std::streamoff(block_size()),std::ios_base::end);
+    std::streamoff cumul=0;
+    for (BlockElt y=0; y<block_size(); ++y)
+    { cumul+= 4*std::streamoff(read_bytes<4>(matrix_file));
+      row_pos[y] = cumul;
+    }
+  }
+
+  else // read old file format, scanning the whole file
+  {
+    size_t l=0; // length of y
+    matrix_file.seekg(0,std::ios_base::beg);
+    for (BlockElt y=0; y<block.size; ++y)
+    {
+      while (y>=block.start_length[l+1]) ++l;
+
+      if (read_bytes<4>(matrix_file)!=y and y!=0)
+      { std::cerr << y << std::endl;
+        throw std::runtime_error ("Alignment problem");
+      }
+      row_pos[y]= matrix_file.tellg(); // record position after row number
+      size_t n_prim=read_bytes<4>(matrix_file);
+      {
+	const prim_list& weak_prims = block.prims_for_descents_of(y);
+	prim_list::const_iterator i= // find limit of |length<l| values
+	  std::lower_bound(weak_prims.begin(),weak_prims.end()
+			  ,block.start_length[l]);
+
+	if (n_prim!=size_t(i-weak_prims.begin())+1)
+        { std::cerr << y << std::endl;
+          throw std::runtime_error ("Primitive count problem");
+        }
+      }
+
+      { size_t n_strong_prim=0;
+        { // compute number of entries to skip, while reading bitmap
+	  static const unsigned int ulsize=sizeof(unsigned long int);
+	  size_t count=n_prim/(8*ulsize);
+	    // number of |unsigned long int|s to read
+
+	  while (count-->0)
+	    n_strong_prim+=bits::bitCount(read_bytes<ulsize>(matrix_file));
+
+	  count=(n_prim%(8*ulsize)+31)/32; // maybe some tetra-byte(s) left
+	  while (count-->0)
+	    n_strong_prim+=bits::bitCount(read_bytes<4>(matrix_file));
+	}
+
+	// now skip over matrix entries
+	matrix_file.seekg(4*std::streamoff(n_strong_prim)
+			 ,std::ios_base::cur);
+      }
+      if (not matrix_file.good())
+      { std::cerr << y << std::endl;
+	throw std::runtime_error ("Premature end of file");
+      }
+    } // for (BlockElt y...)
+  } // if (...==magic_code)
+
+  block_file.close(); // success, we no longer need the block file
+}
+
+
+
+   // Functions declared in filekl.h
+
+/* The following function is an alternative to the function |wGraph| defined
+   in kl.cpp. Here we do not assume that a KLContext is available, but that
+   binary files with information about the block, matrix, and KL polynomials
+   are avaialble. As a consequence we must redo the work of
+   |kl::Helper::fillMuRow| as well as that to the mentioned |wGraph| function.
+*/
+
+wgraph::WGraph wGraph
+  ( std::ifstream& block_file
+  , std::ifstream& matrix_file
+  , std::ifstream& KL_file)
+{
+  matrix_info mi(block_file,matrix_file);
+  polynomial_info poli(KL_file);
+
+  BlockElt dummy;
+
+  size_t max_mu=1;                       // maximal mu found
+  std::pair<BlockElt,BlockElt> max_pair; // corresponding (x,y)
+
+  wgraph::WGraph result(mi.rank()); result.resize(mi.block_size());
+
+  // fill in descent sets
+  for (BlockElt y = 0; y < mi.block_size(); ++y)
+  {
+    RankFlags d_y = result.descent(y) = mi.descent_set(y);
+    size_t ly = mi.length(y);
+    if (ly==0) continue; // nothing more to do; avoid negative |d| below
+
+#ifdef VERBOSE
+    std::cerr << "reading edges for y=" << y;
+    if (max_mu>1) std::cerr << ", maximal mu: " << max_mu;
+    std::cerr << '\r';
+#endif
+
+    const strong_prim_list& spy=mi.strongly_primitives(y);
+
+    strong_prim_list::const_iterator start= spy.begin();
+    // traverse lengths |lx| of opposite parity to |ly|, up to |ly-3|
+    for (size_t lx=(ly-1)%2,d = (ly-1)/2; d>0; --d,lx+=2) // d=(ly-1-lx)/2
+    {
+      strong_prim_list::const_iterator stop =
+	std::lower_bound(start,spy.end()-1,mi.first_of_length(lx+1));
+      for (start= std::lower_bound(start,stop,mi.first_of_length(lx));
+	   start<stop; ++start)
+	if (mi.descent_set(*start)!=d_y)
+        {
+	  BlockElt x = *start;
+	  KLIndex klp = mi.find_pol_nr(x,y,dummy);
+	  if (poli.degree(klp)==d)
+	  {
+	    result.edgeList(x).push_back(y);
+	    size_t mu=poli.leading_coeff(klp);
+	    if (mu>max_mu) { max_mu=mu; max_pair=std::make_pair(x,y); }
+	    result.coeffList(x).push_back(mu);
+	  }
+	} // for (start...) if (descent!=d_y)
+    } // for (lx,d)
+
+    // for length |ly-1| we cannot limit ourselves to strongly primitives
+    BlockElt end=mi.first_of_length(ly);
+    for (BlockElt x=mi.first_of_length(ly-1); x<end; ++x)
+    {
+      RankFlags d_x=mi.descent_set(x);
+      if (d_x==d_y) continue; // this case would lead nowhere anyway
+      KLIndex klp = mi.find_pol_nr(x,y,dummy);
+      if (klp!=KLIndex(0)) // then some edge between |x| and |y| exists
+      {
+	size_t mu=poli.leading_coeff(klp);
+	if (mu>max_mu) { max_mu=mu; max_pair=std::make_pair(x,y); }
+	if (not d_y.contains(d_x))
+	{
+	  result.edgeList(x).push_back(y);
+	  result.coeffList(x).push_back(mu);
+	}
+	if (not d_x.contains(d_y))
+	{
+	  result.edgeList(y).push_back(x);
+	  result.coeffList(y).push_back(mu);
+	}
+      } // if (klp!=KLIndex(0))
+    } // for (x)
+  } // for (y)
+
+  if (max_mu==1) std::cout << "All edges found are simple.\n";
+  else std::cout << "Maximal edge multiplicity " << max_mu << " for ("
+		 << max_pair.first << ',' << max_pair.second <<")\n";
+
+  return result;
+}
+
+
+} // namespace filekl
+} // namespace atlas
---
+++ sources/utilities/filekl.h
@@ -0,0 +1,153 @@
+/*!
+\file
+  This is filekl.h
+*/
+/*
+  Copyright (C) 2007 Marc van Leeuwen
+  part of the Atlas of Reductive Lie Groups
+
+  See file main.cpp for full copyright notice
+*/
+
+
+#ifndef FILEKL_H
+#define FILEKL_H
+
+#include <iostream>
+#include <fstream>
+
+#include "bitset_fwd.h"
+#include "blocks.h"
+#include "wgraph.h"
+
+namespace atlas {
+
+  namespace filekl {
+
+
+    // The |polynomial_info| class
+
+typedef unsigned long long int ullong; // a 64-bit type even on 32-bit machines
+
+typedef ullong KLIndex;
+ // here it must be |long long| to avoid problems when multiplied
+
+// A class giving access to the polynomial file (which remains open)
+class polynomial_info
+{
+  std::ifstream& file; // non-owned reference to (open) file
+
+  KLIndex n_pols;         // number of polynomials in file
+  unsigned int coef_size; // number of bytes per coefficient
+  ullong n_coef;          // number of coefficients
+  std::streamoff index_begin, coefficients_begin;
+
+public:
+  polynomial_info(std::ifstream& coefficient_file);
+
+  KLIndex n_polynomials() const { return n_pols; }
+  unsigned int coefficient_size() const { return coef_size; }
+  ullong n_coefficients() const { return n_coef; }
+
+  size_t degree(KLIndex i) const;
+  std::vector<size_t> coefficients(KLIndex i) const;
+  size_t leading_coeff(KLIndex i) const;
+};
+
+    // The |block_info| class
+
+using blocks::BlockElt;
+using bitset::RankFlags;
+
+typedef std::vector<RankFlags> descent_set_vector; // indexed by block element
+
+typedef std::vector<BlockElt> ascent_vector;       // indexed by simple root
+typedef std::vector<ascent_vector> ascent_table;   // indexed by block element
+
+typedef std::vector<BlockElt> prim_list;           // list of weak primitives
+typedef std::vector<prim_list> prim_table;
+
+
+struct block_info
+{
+  unsigned int rank;
+  BlockElt size;
+  unsigned int max_length; // maximal length of block elements
+  std::vector<BlockElt> start_length;
+   // array has size |max_length+2|; it defines intervals for each length
+
+  descent_set_vector descent_set;     // descent (bit)set listed per BlockElt
+
+private:
+  ascent_table ascents;       // raised element per simple root, per BlockElt
+  prim_table primitives_list; // lists of weakly primitives, per descent set
+
+public:
+  block_info(std::ifstream& in); // constructor reads, and closes, file
+
+  BlockElt primitivize(BlockElt x, BlockElt y) const;
+  const prim_list& prims_for_descents_of(BlockElt y);
+private:
+  bool is_primitive(BlockElt x, const RankFlags d) const;
+  void compute_prim_table();
+};
+
+
+
+    // the |matrix_info| class
+
+typedef prim_list strong_prim_list;
+
+
+class matrix_info
+{
+  std::ifstream& matrix_file; // non-owned reference to (open) file
+
+  block_info block;
+
+  std::vector<std::streampos> row_pos; // positions where each row starts
+
+// data for currently selected row~|y|
+  BlockElt cur_y;		       // row number
+  strong_prim_list cur_strong_prims;   // strongly primitives for this row
+  std::streampos cur_row_entries; // indices of polynomials for row start here
+
+//private methods
+  matrix_info(const matrix_info&); // copying forbidden
+  void set_y(BlockElt y);          // install |cur_y| and dependent data
+
+public:
+
+// constructor and destructor
+  matrix_info(std::ifstream& block_file,std::ifstream& m_file);
+  ~matrix_info() {}
+
+// accessors
+  size_t rank() const { return block.rank; }
+  BlockElt block_size() const { return block.size; }
+  size_t length (BlockElt y) const; // length in block
+  BlockElt first_of_length (size_t l) const { return block.start_length[l]; }
+  RankFlags descent_set (BlockElt y) const { return block.descent_set[y]; }
+  std::streamoff row_offset(BlockElt y) const { return row_pos[y]; }
+  BlockElt primitivize (BlockElt x,BlockElt y) const
+    { return block.primitivize(x,y); }
+
+// manipulators (they are so because they set |cur_y|)
+  KLIndex find_pol_nr(BlockElt x,BlockElt y,BlockElt& xx);
+  BlockElt prim_nr(unsigned int i,BlockElt y);// find primitive element
+  const strong_prim_list& strongly_primitives (BlockElt y)
+    { set_y(y); return cur_strong_prims; } // changing |y| invalidates this!
+};
+
+// Functions
+
+wgraph::WGraph wGraph
+  ( std::ifstream& block_file
+  , std::ifstream& matrix_file
+    , std::ifstream& KL_file);
+
+} // namespace filekl
+
+} // namespace atlas
+
+#endif
