22e5b191ec2e574365a04d6970ace5af115943c1
mvl <mvl>
2007-10-29 16:33:07 +0000

    Installed some missing bit and pieces of help information.

--- sources/interface/mainhelp.cpp
+++ sources/interface/mainhelp.cpp
@@ -1,8 +1,8 @@
 /*
   This is mainhelp.cpp
-  
+
   Copyright (C) 2004,2005 Fokko du Cloux
-  part of the Atlas of Reductive Lie Groups 
+  part of the Atlas of Reductive Lie Groups
 
   See file main.cpp for full copyright notice
 */
@@ -28,34 +28,35 @@ namespace atlas {
 namespace {
 
   // help commands
-  void blocksizes_h();
-  void coroots_h();
-  void poscoroots_h();
-  void posroots_h();
+  void type_h();
+  void simpleroots_h();
+  void simplecoroots_h();
   void roots_h();
+  void coroots_h();
+  void posroots_h();
+  void poscoroots_h();
   void showdualforms_h();
   void showrealforms_h();
-  void simplecoroots_h();
-  void simpleroots_h();
-  void type_h();
+  void blocksizes_h();
+  void gradings_h();
 
   // command tags for the help facility
-  const char* blocksizes_tag = "outputs the matrix of blocksizes";
-  const char* coroots_tag = "outputs the coroots in the lattice basis";
-  const char* poscoroots_tag = 
-    "outputs the positive coroots in the lattice basis";
-  const char* posroots_tag = "outputs the positive roots in the lattice basis";
-  const char* roots_tag = "outputs the roots in the lattice basis";
-  const char* showdualforms_tag = 
-    "outputs the weak real forms for the dual group";
-  const char* showrealforms_tag = 
-    "outputs the weak real forms for this complex group";
-  const char* simplecoroots_tag = 
-    "outputs the simple coroots in the lattice basis";
-  const char* simpleroots_tag = 
-    "outputs the simple roots in the lattice basis";
   const char* type_tag = "resets the group type";
-
+  const char* simpleroots_tag =
+    "outputs the simple roots in the lattice basis";
+  const char* simplecoroots_tag =
+    "outputs the simple coroots in the lattice basis";
+  const char* roots_tag = "outputs the roots in the lattice basis";
+  const char* coroots_tag = "outputs the coroots in the lattice basis";
+  const char* posroots_tag = "outputs the positive roots in the lattice basis";
+  const char* poscoroots_tag =
+    "outputs the positive coroots in the lattice basis";
+  const char* showdualforms_tag =
+    "outputs the weak real forms for the dual group";
+  const char* showrealforms_tag =
+    "outputs the weak real forms for this complex group";
+  const char* blocksizes_tag = "outputs the matrix of blocksizes";
+  const char* gradings_tag = "shows the gradings of the imaginary root system";
 }
 
 /*****************************************************************************
@@ -68,30 +69,32 @@ namespace mainhelp {
 
 void addMainHelp(commands::CommandMode& mode, commands::TagDict& tagDict)
 
-{  
+{
   using namespace commands;
 
-  mode.add("blocksizes",blocksizes_h);
-  mode.add("coroots",coroots_h);
-  mode.add("poscoroots",poscoroots_h);
-  mode.add("posroots",posroots_h);
-  mode.add("roots",roots_h);
-  mode.add("showdualforms",showdualforms_h);
-  mode.add("showrealforms",showrealforms_h);
+  mode.add("type",type_h);
   mode.add("simpleroots",simpleroots_h);
   mode.add("simplecoroots",simplecoroots_h);
-  mode.add("type",type_h);
+  mode.add("roots",roots_h);
+  mode.add("coroots",coroots_h);
+  mode.add("posroots",posroots_h);
+  mode.add("poscoroots",poscoroots_h);
+  mode.add("showdualforms",showdualforms_h);
+  mode.add("showrealforms",showrealforms_h);
+  mode.add("blocksizes",blocksizes_h);
+  mode.add("gradings",gradings_h);
 
-  insertTag(tagDict,"blocksizes",blocksizes_tag);
+  insertTag(tagDict,"type",type_tag);
+  insertTag(tagDict,"simpleroots",simpleroots_tag);
+  insertTag(tagDict,"simplecoroots",simplecoroots_tag);
+  insertTag(tagDict,"roots",roots_tag);
   insertTag(tagDict,"coroots",coroots_tag);
   insertTag(tagDict,"poscoroots",poscoroots_tag);
   insertTag(tagDict,"posroots",posroots_tag);
-  insertTag(tagDict,"roots",roots_tag);
   insertTag(tagDict,"showdualforms",showdualforms_tag);
   insertTag(tagDict,"showrealforms",showrealforms_tag);
-  insertTag(tagDict,"simpleroots",simpleroots_tag);
-  insertTag(tagDict,"simplecoroots",simplecoroots_tag);
-  insertTag(tagDict,"type",type_tag);
+  insertTag(tagDict,"blocksizes",blocksizes_tag);
+  insertTag(tagDict,"gradings",gradings_tag);
 
   return;
 }
@@ -102,80 +105,76 @@ void addMainHelp(commands::CommandMode& mode, commands::TagDict& tagDict)
 
         Chapter II --- Functions for the help commands
 
-  This section contains the definitions of the help functions associated to 
+  This section contains the definitions of the help functions associated to
   the various commands defined in this mode.
 
 ******************************************************************************/
 
 namespace {
 
-void blocksizes_h()
+void type_h()
+{
+  io::printFile(std::cerr,"type.help",io::MESSAGE_DIR);
+  return;
+}
 
-{  
-  io::printFile(std::cerr,"blocksizes.help",io::MESSAGE_DIR);
+void simpleroots_h()
+{
+  io::printFile(std::cerr,"simpleroots.help",io::MESSAGE_DIR);
+  return;
+}
+
+void simplecoroots_h()
+{
+  io::printFile(std::cerr,"simplecoroots.help",io::MESSAGE_DIR);
+  return;
+}
+
+void roots_h()
+{
+  io::printFile(std::cerr,"roots.help",io::MESSAGE_DIR);
   return;
 }
 
 void coroots_h()
-
-{  
+{
   io::printFile(std::cerr,"coroots.help",io::MESSAGE_DIR);
   return;
 }
 
 void posroots_h()
-
-{  
+{
   io::printFile(std::cerr,"posroots.help",io::MESSAGE_DIR);
   return;
 }
 
 void poscoroots_h()
-
-{  
+{
   io::printFile(std::cerr,"poscoroots.help",io::MESSAGE_DIR);
-  return;
-}
-   
-void roots_h()
-
-{  
-  io::printFile(std::cerr,"roots.help",io::MESSAGE_DIR);
   return;
 }
 
 void showdualforms_h()
-  
-{  
+{
   io::printFile(std::cerr,"showdualforms.help",io::MESSAGE_DIR);
   return;
 }
 
 void showrealforms_h()
-  
-{  
+{
   io::printFile(std::cerr,"showrealforms.help",io::MESSAGE_DIR);
   return;
 }
 
-void simpleroots_h()
-
-{  
-  io::printFile(std::cerr,"simpleroots.help",io::MESSAGE_DIR);
-  return;
-}
-  
-void simplecoroots_h()
-
-{  
-  io::printFile(std::cerr,"simplecoroots.help",io::MESSAGE_DIR);
-  return;
-}
-
-void type_h()
-
+void blocksizes_h()
 {
-  io::printFile(std::cerr,"type.help",io::MESSAGE_DIR);
+  io::printFile(std::cerr,"blocksizes.help",io::MESSAGE_DIR);
+  return;
+}
+
+void gradings_h()
+{
+  io::printFile(std::cerr,"gradings.help",io::MESSAGE_DIR);
   return;
 }
 
--- sources/interface/realhelp.cpp
+++ sources/interface/realhelp.cpp
@@ -1,8 +1,8 @@
 /*
   This is realhelp.cpp
-  
+
   Copyright (C) 2004,2005 Fokko du Cloux
-  part of the Atlas of Reductive Lie Groups 
+  part of the Atlas of Reductive Lie Groups
 
   See file main.cpp for full copyright notice
 */
@@ -28,12 +28,14 @@ namespace {
 
   // help commands
   void cartan_h();
+  void gradings_h();
   void realform_h();
   void realweyl_h();
   void strongreal_h();
 
   // command tags for the help facility
   const char* cartan_tag = "prints the conjugacy classes of Cartan subgroups";
+  const char* gradings_tag ="print gradings of imaginary roots for real forms";
   const char* realform_tag = "sets the real form for the group";
   const char* realweyl_tag = "outputs the structure of the real Weyl group";
   const char* strongreal_tag = "outputs information about strong real forms";
@@ -50,15 +52,17 @@ namespace realhelp {
 
 void addRealHelp(commands::CommandMode& mode, commands::TagDict& tagDict)
 
-{  
+{
   using namespace commands;
 
-  mode.add("cartan",&cartan_h);
+  mode.add("cartan",cartan_h);
+  mode.add("gradings",gradings_h);
   mode.add("realform",realform_h);
   mode.add("realweyl",realweyl_h);
   mode.add("strongreal",strongreal_h);
 
   insertTag(tagDict,"cartan",cartan_tag);
+  insertTag(tagDict,"gradings",gradings_tag);
   insertTag(tagDict,"realform",realform_tag);
   insertTag(tagDict,"realweyl",realweyl_tag);
   insertTag(tagDict,"strongreal",strongreal_tag);
@@ -72,7 +76,7 @@ void addRealHelp(commands::CommandMode& mode, commands::TagDict& tagDict)
 
         Chapter II --- Functions for the help commands
 
-  This section contains the definitions of the help functions associated to 
+  This section contains the definitions of the help functions associated to
   the various commands defined in this mode.
 
 ******************************************************************************/
@@ -80,24 +84,24 @@ void addRealHelp(commands::CommandMode& mode, commands::TagDict& tagDict)
 namespace {
 
 void cartan_h()
-
 {
   io::printFile(std::cerr,"cartan.help",io::MESSAGE_DIR);
-  return;
+}
+
+void gradings_h()
+{
+  io::printFile(std::cerr,"gradings.help",io::MESSAGE_DIR);
 }
 
 void realform_h()
-
 {
   io::printFile(std::cerr,"realform.help",io::MESSAGE_DIR);
-  return;
 }
 
 void realweyl_h()
 
 {
   io::printFile(std::cerr,"realweyl.help",io::MESSAGE_DIR);
-  return;
 }
 
 
@@ -105,9 +109,8 @@ void strongreal_h()
 
 {
   io::printFile(std::cerr,"strongreal.help",io::MESSAGE_DIR);
-  return;
 }
 
-}
+} // namespace
 
-}
+} // namespace atlas
