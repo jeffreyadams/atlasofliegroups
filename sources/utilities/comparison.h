7a90fa6423e04d23fe4e083f1a6d52a63032ccd9
mvl <mvl>
2008-09-26 13:34:23 +0000

    Corrected a bad bug in Compare (constructor takes a by-value argument to which the class stores a pointer) that has remained dormant in the code for years!

--- sources/utilities/comparison.h
+++ sources/utilities/comparison.h
@@ -4,7 +4,7 @@
 */
 /*
   Copyright (C) 2004,2005 Fokko du Cloux
-  part of the Atlas of Reductive Lie Groups 
+  part of the Atlas of Reductive Lie Groups
 
   For license information see the LICENSE file
 */
@@ -24,21 +24,34 @@ namespace comparison {
 
 namespace comparison {
 
-template<typename F>
-  inline Compare<F> compare(F f) {return Compare<F>(f);}
+/* The template Compare turns a unary function object into a comparison class
+   The comparison is done by comparing the images under the function object
+   by means of operator<.
 
+   So if |f| is a unary function object, one can set
+
+   Compare<F> comp=Compare<F>(f); // or Compare<F> comp(f);
+
+   after which |comp(x,y)| is equivalent to |f(x)<f(y)|. Rather than being
+   stored in a variable, |Compare<F>(f)| is usually passed as an argument (of
+   type |const Compare<F>&|) to a (STL) function needing a comparison object.
+ */
+
+// template function allowing |compare(f)| to abbreviate |Compare<F>(f)|
 template<typename F>
+  inline Compare<F> compare(const F& f) { return Compare<F>(f);}
+
+template<typename F> // F is the type of a unary function object
 class Compare {
  private:
-  F* d_f;
+  const F& d_f;
  public:
-  explicit Compare(F f):d_f(&f) {}
+  explicit Compare(const F& f):d_f(f) {}
 
-  bool operator() (typename F::argument_type x, 
-		   typename F::argument_type y) const {
-    typename F::result_type lhs = (*d_f)(x);
-    typename F::result_type rhs = (*d_f)(y);
-    return  lhs < rhs;
+  bool operator() (typename F::argument_type x,
+		   typename F::argument_type y) const
+  {
+    return  d_f(x) < d_f(y); // F::result_type should define operator<
   }
 };
 
