1f6974decef5a533809c2518f1d3f4122f44a89f
mvl <mvl>
2009-04-14 13:30:25 +0000

    Split off layout_fwd.h for declaring lietype::involution, avoiding include loop

---
+++ sources/io/layout_fwd.h
@@ -0,0 +1,17 @@
+/*
+  This is layout.h
+
+  Copyright (C) 2009 Marc van Leeuwen
+  part of the Atlas of Reductive Lie Groups
+
+  For license information see the LICENSE file
+*/
+
+#ifndef LAYOUT_FWD_H  /* guard against multiple inclusions */
+#define LAYOUT_FWD_H
+
+namespace atlas { namespace layout {
+struct Layout;
+}}
+
+#endif
