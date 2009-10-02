6715cb57620b5d2f81430b14ed5f4fc7b96fbb0d
mvl <mvl>
2009-10-02 15:27:39 +0000

    Cleaning up of code and comments, without any change for using code

--- sources/structure/latticetypes_fwd.h
+++ sources/structure/latticetypes_fwd.h
@@ -2,17 +2,12 @@
 \file
 \brief Forward declarations of classes and types for namespace latticetypes.
 
-A LatticeCoeff is an integer.  A LatticeElt is a vector of
-LatticeCoeff's; that is, an element of Z^n.  A Weight is a LatticeElt.
-The LatticeElt's now used are mostly of dimension RANK_MAX (now set at
-16).
-
-A RatLatticeElt is a class corresponding to an element of Q^n.
 */
 /*
   This is latticetypes_fwd.h
 
   Copyright (C) 2004,2005 Fokko du Cloux
+  Copyright (C) 2009 Marc van Leeuwen
   part of the Atlas of Reductive Lie Groups
 
   For license information see the LICENSE file
@@ -35,30 +30,14 @@ namespace atlas {
 
 namespace latticetypes {
 
-  /*!
-  \brief
-  A LatticeElt is a vector of LatticeCoeff's; that is, an element of
-  Z^n.
-  */
-  typedef int LatticeCoeff;
+  typedef int LatticeCoeff; // the instance type of |Vector| and |Matrix|
 
-  const LatticeCoeff ZeroCoeff = 0;
-  const LatticeCoeff OneCoeff = 1;
-
-
-  /*!
-  \brief
-  A LatticeElt is a vector of LatticeCoeff's; that is, an element of
-  Z^n.
-  */
   typedef matrix::Vector<LatticeCoeff> LatticeElt;
 
-  /*!
-  \brief
-  This type of list of integers is used as the list of invariant
-  factors in a Smith normal form.
-  */
-  typedef std::vector<LatticeCoeff> CoeffList;
+  typedef std::vector<LatticeCoeff> CoeffList; // no vector arithmetic here
+
+  typedef matrix::Matrix<LatticeCoeff> LatticeMatrix;
+
   class RatLatticeElt;
 
   typedef LatticeElt Weight;
@@ -67,76 +46,25 @@ namespace latticetypes {
   typedef std::vector<Weight> WeightList;
   typedef std::vector<RatWeight> RatWeightList;
 
-  typedef matrix::Matrix<LatticeCoeff> LatticeMatrix;
-
-  /*!
-  \brief Element of (Z/2Z)^RANK_MAX.
-
-  Used to represent an element of a component group of a real torus;
-  this is why it turns up in connection with lattices.
-  */
-  typedef bitvector::BitVector<constants::RANK_MAX> Component;
-
-  /* In Fokko's code, the type |Component| hardly ever represents an element
-     of the component group of a real torus (or other variety). For this
-     reason I've introduced a more neutral name to replace such uses. MvL */
-
   typedef bitvector::BitVector<constants::RANK_MAX> SmallBitVector;
 
   typedef bitvector::BitVectorList<constants::RANK_MAX> SmallBitVectorList;
-
-  /* In Fokko's code, the type |LongComponent| is used (exclusively?) to
-     represent a line of an equation matrix over $Z/2Z$, with a left hand side
-     of at most |RANK_MAX| coefficient bits, and one bit right hand side. For
-     this reason I've introduced a more appropirate type for such uses. MvL */
 
   typedef bitvector::BitVector<constants::RANK_MAX+1> BinaryEquation;
 
   typedef bitvector::BitVectorList<constants::RANK_MAX+1> BinaryEquationList;
 
-  /*!
-  \brief Element of (Z/2Z)^2*RANK_MAX.
-  */
-  typedef bitvector::BitVector<2*constants::RANK_MAX> LongComponent;
-
-  /*!
-  \brief Square matrix of size RANK_MAX with entries in Z/2Z.
-
-  Used to represent for instance the map on (dual) component groups of real
-  tori induced by a lattice map.
-  */
+  //! \brief Square matrix of size at most RANK_MAX with entries in Z/2Z.
   typedef bitvector::BitMatrix<constants::RANK_MAX> BinaryMap;
 
-  /*!
-  \brief Subgroup of (Z/2Z)^RANK_MAX.
-
-  Used to represent a subgroup of the group of connected components of
-  a real torus.
-  */
+  //! \brief Subgroup of (Z/2Z)^RANK_MAX.
   typedef subquotient::Subspace<constants::RANK_MAX> SmallSubspace;
 
+  //! \brief Subquotient of (Z/2Z)^RANK_MAX.
+  typedef subquotient::Subquotient<constants::RANK_MAX> SmallSubquotient;
 
-  /*!
-  \brief Subquotient of (Z/2Z)^RANK_MAX.
-  */
-  typedef subquotient::Subquotient<constants::RANK_MAX>
-    SmallSubquotient;
+} // |namespace latticetypes|
 
-  /*!
-  \brief List of elements of (Z/2Z)^RANK_MAX.
-
-  Used to represent a subset of the group of connected components of
-  a real torus.
-  */
-  typedef std::vector<Component> ComponentList;
-
-  /*!
-  \brief List of elements of (Z/2Z)^2*RANK_MAX.
-  */
-  typedef std::vector<LongComponent> LongComponentList;
-
-}
-
-}
+} // |namespace atlas|
 
 #endif
