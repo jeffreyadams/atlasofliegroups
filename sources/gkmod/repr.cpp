jeffreyadams/atlasofliegroups

commit 970b4b8258752e27f5bee39eec335046ac5528bc

Modify SubSytem, extending conjugation-to-simple info to all subsys posroots
Use this to streamline Rep_context::any_Cayley and ext_block::type

sources/gkmod/repr.cpp
@@ -536,7 +536,7 @@ StandardRepr Rep_context::inv_Cayley(weyl::Generator s, StandardRepr z) const
 
 /* compute shift in |lambda| component of parameter for Cayley transform
    by  non-simple root $\alpha$, from involutions |theta_down| to |theta_up|,
-   where |ww| right-conjugates root $\alpha$ to some simple root $\beta$
+   where |ww| left-conjugates root $\alpha$ to some simple root $\beta$
 
    Sum of positve roots changing real status, and becoming negative at $\beta$
 */
@@ -549,7 +549,7 @@ Weight Cayley_shift (const ComplexReductiveGroup& G,
     (i_tab.real_roots(theta_up)^i_tab.real_roots(theta_down))&rd.posRootSet();
   Weight shift(rd.rank(),0); // difference of rho values
   for (auto it=new_real_posroots.begin(); it(); ++it)
-    if (rd.is_negroot(rd.permuted_root(*it,to_simple)))
+    if (rd.is_negroot(rd.permuted_root(to_simple,*it)))
       shift += rd.root(*it); // sum posroots changing "real" and (by w) "pos"
   return shift;
 }
@@ -561,35 +561,36 @@ StandardRepr Rep_context::any_Cayley(const Weight& alpha, StandardRepr z) const
   const InvolutionTable& i_tab = complexGroup().involution_table();
   const SubSystem& subsys = SubSystem::integral(rd,z.infinitesimal_char);
 
+  // prepare: move to a situation with integrally dominant infinitesimal char.
   WeylWord w=make_dominant(z,subsys);
   KGBElt x= z.x_part; // take a working copy; don't disturb |z|
   Weight lr = lambda_rho(z); // use at end to build new parameter
   const RatWeight& infin_char=z.infinitesimal_char; // constant from here on
 
-  RootNbr rt = rd.root_index(alpha);
-  if (rt==rd.numRoots())
-    throw std::runtime_error("Not a root");
-  make_positive(rd,rt);
-  const RootNbrSet posroots_sub = subsys.positive_roots();
-  if (not posroots_sub.isMember(rt))
+  // check the root argument, and if OK make the corresponding move to above
+  RootNbr rt = subsys.from_parent(rd.root_index(alpha)); // |subsys| numbering
+  if (rt == ~ RootNbr(0)) // either not a root at all or not in subsystem
     throw std::runtime_error("Not an integral root");
   // apply the integrally-dominant-making $W$ element |w| (in |subsys|) to |rt|:
-  rt=subsys.to_parent(subsys.permuted_root(subsys.from_parent(rt),w));
+  rt = subsys.permuted_root(rt,w); // now we've got the root to do Cayley by
+  rt = subsys.rt_abs(rt); // henceforth interpret as index of a positive root
 
+  // now do the Cayley transform proper
   InvolutionNbr inv0= kgb.inv_nr(x);
-  RootNbr s=rt; // future simple root index, the one that |rt| conjugates to
-  WeylWord ww = conjugate_to_simple(rd,s);
-  x = kgb.cross(x,ww);
+  weyl::Generator s=subsys.simple(rt);
+  WeylWord ww = subsys.to_simple(rt);
+  x = kgb.cross(ww,x);
   switch (kgb.status(s,x))
   {
   case gradings::Status::ImaginaryNoncompact:
     x = kgb.cayley(s,x); break;
-  case gradings::Status::Real:
+  case gradings::Status::Real: // find out (at inv0) whether root is parity
     { Weight rho2_diff = rd.twoRho() - rd.twoRho(i_tab.real_roots(inv0));
       RatWeight parity_vector = // compute this at the \emph{original} x
 	infin_char - lr - RatWeight(std::move(rho2_diff),2);
-      if (parity_vector.dot(rd.coroot(rt))%2!=0) // then |rt| was parity
-      {	x = kgb.inverseCayley(s,x).first; // do inverse Cayley at |inv1|
+      if (parity_vector.dot(rd.coroot(subsys.to_parent(rt)))%2!=0)
+      { // then |rt| was parity
+	x = kgb.inverseCayley(s,x).first; // do inverse Cayley at |inv1|
 	break;
       }
       // else FALL THROUGH
@@ -598,7 +599,7 @@ StandardRepr Rep_context::any_Cayley(const Weight& alpha, StandardRepr z) const
   case gradings::Status::Complex:
     throw error::Cayley_error();
   }
-  x = kgb.cross(ww,x); // finally cross back
+  x = kgb.cross(x,ww); // finally cross back
 
   lr += Cayley_shift(complexGroup(),inv0,kgb.inv_nr(x),ww); // apply shift
   z = sr_gamma(x,lr,infin_char);

sources/gkmod/ext_block.cpp
@@ -656,15 +656,13 @@ DescValue type (const param& E, ext_gen p, std::vector<param>& cross_links)
 	  if (matreduc::has_solution
 	      (i_tab.matrix(theta).transposed()+1, alpha_v)) // type 2
 	  {// now find out if the Cayley transforms are delta-fixed
-	    TwistedInvolution new_tw = W.prod(E.tw,subs.reflection(p.s0));
-	    WeylWord ww = subs.to_simple(p.s0);
-	    std::reverse(ww.begin(),ww.end()); // awkward, for |Cayley_shift|
-            Weight new_lambda = E.lambda_rho + repr::Cayley_shift
+	    const TwistedInvolution new_tw = W.prod(E.tw,subs.reflection(p.s0));
+	    const WeylWord ww = subs.to_simple(p.s0);
+            const Weight new_lambda = E.lambda_rho + repr::Cayley_shift
 	      (E.rc().complexGroup(),i_tab.nr(E.tw),i_tab.nr(new_tw),ww);
-	    new_lambda -= E.ctxt.delta()*new_lambda; // change by $\delta$
-	    result = // was |new_lambda| |delta|-fixed mod $(1-\theta')X^*$?
+	    result = // is |new_lambda| |delta|-fixed modulo $(1-\theta')X^*$?
 	      matreduc::has_solution(i_tab.matrix(new_tw)-1,
-				     std::move(new_lambda))
+				     (E.ctxt.delta()-1)*new_lambda)
 	      ? one_imaginary_pair_fixed : one_imaginary_pair_switched;
 	  }
 	  else // type 1

sources/structure/subsystem.cpp
@@ -29,14 +29,27 @@ SubSystem::SubSystem(const RootDatum& parent,
   , rd(parent) // share
   , pos_map(numPosRoots(),~0)
   , inv_map(rd.numRoots()+1,~0) // one spare entry for "unfound root in parent"
-  , sub_root(sub_sys.size())
+  , sub_root(numPosRoots())
 {
-  for (weyl::Generator i=0; i<sub_sys.size(); ++i)
+  for (unsigned int i=0; i<numPosRoots(); ++i)
   {
-    RootNbr alpha = pos_map[i]=sub_sys[i];
-    inv_map[alpha] = simpleRootNbr(i); // point to simple root |i| in subsystem
+    // first set |pos_map[i]|
+    if (i<sub_sys.size()) // simple in subsystem, just use |sub_sys|
+      pos_map[i]=sub_sys[i];
+    else // use previously stored values in |pos_map|, |sub_root| recursively
+    {
+      RootNbr sub_alpha = posRootNbr(i);
+      weyl::Generator s = find_descent(sub_alpha); // generator for subsystem
+      simple_reflect_root(s,sub_alpha);
+      RootNbr beta = pos_map[posRootIndex(sub_alpha)]; // |beta| is in parent
+      pos_map[i] = rd.permuted_root(sub_root[s].reflection,beta);
+    }
+
+    RootNbr alpha = pos_map[i]; // now we use parent numbering
+    inv_map[alpha] = posRootNbr(i); // refers simple root |i| in subsystem
     inv_map[rd.rootMinus(alpha)] = rootMinus(inv_map[alpha]); // its negative
 
+    // in the remainder we work in parent datum; must find conjugate to simple
     size_t count=0; weyl::Generator s;
     while (alpha!=rd.simpleRootNbr(s=rd.find_descent(alpha)))
     { // just count the reflections needed to make alpha simple
@@ -44,30 +57,20 @@ SubSystem::SubSystem(const RootDatum& parent,
       ++count;
     }
 
+    // now we can dimension our Weyl words, and set |sub_root[i].simple|
     sub_root[i].to_simple.resize(count);
     sub_root[i].reflection.resize(2*count+1);
     sub_root[i].simple=sub_root[i].reflection[count]=s; // set middle letter
 
-    size_t j=count; // redo search loop, now storing values
-    for (alpha=sub_sys[i]; j-->0; rd.simple_reflect_root(s,alpha))
+    size_t j=count; // redo search loop, storing remaining Weyl word letters
+    for (alpha=pos_map[i]; j-->0; rd.simple_reflect_root(s,alpha))
     {
       s=rd.find_descent(alpha);
       sub_root[i].to_simple[j]=s; // write |to_simple| word from right to left
       sub_root[i].reflection[count+1+j]= // and |reflection| from outside in
       sub_root[i].reflection[count-1-j]=s;
     }
-    assert(alpha==rd.simpleRootNbr(sub_root[i].simple));
-  }
-
-  for (unsigned int i=rank(); i<numPosRoots(); ++i)
-  {
-    RootNbr alpha = posRootNbr(i); // root number in subsystem
-    weyl::Generator s = find_descent(alpha);
-    simple_reflect_root(s,alpha);
-    RootNbr beta = pos_map[posRootIndex(alpha)]; // in parent
-    pos_map[i] = rd.permuted_root(sub_root[s].reflection,beta);
-    inv_map[pos_map[i]] = posRootNbr(i);
-    inv_map[rd.rootMinus(pos_map[i])] = numPosRoots()-1-i;
+    assert(alpha==rd.simpleRootNbr(sub_root[i].simple)); // check |alpha|
   }
 }
 

sources/structure/subsystem.h
@@ -41,9 +41,9 @@ class SubSystem : public RootSystem // new system, subsytem of dual
   RootNbrList inv_map; // partial map back from all parent roots
 
   struct root_info
-  { weyl::Generator simple;
-    WeylWord to_simple;
-    WeylWord reflection;
+  { weyl::Generator simple; // some simple root $s$ in parent conjugate to root
+    WeylWord to_simple; // word $w$ conjugating the root to mentioned simple
+    WeylWord reflection; // reflection word for root: $w^{-1}sw$
 
   root_info() : simple(~0), to_simple(), reflection() {}
   };
@@ -82,14 +82,20 @@ class SubSystem : public RootSystem // new system, subsytem of dual
   RootNbr to_parent(RootNbr alpha) const; // |pos_map| with some shifting
   RootNbr from_parent(RootNbr alpha) const { return inv_map[alpha]; }
 
-  weyl::Generator simple(weyl::Generator s) const
-  { return sub_root[s].simple; } // parent simple root conjugated to |sub.s|
+  weyl::Generator simple(unsigned int n) const
+  { assert(n<numPosRoots()); // n must be a positive root number for subsystem
+    return sub_root[n].simple; // parent simple root conjugated to |sub.s|
+  }
 
-  const WeylWord& to_simple(weyl::Generator s) const
-  { return sub_root[s].to_simple; } // parent conjugating word for |simple(s)|
+  const WeylWord& to_simple(unsigned int n) const
+  { assert(n<numPosRoots()); // n must be a positive root number for subsystem
+    return sub_root[n].to_simple; // parent conjugating word for |simple(s)|
+  }
 
-  const WeylWord& reflection(weyl::Generator s) const
-  { return sub_root[s].reflection; } // parent reflection corresponding to |s|
+  const WeylWord& reflection(unsigned int n) const
+  { assert(n<numPosRoots()); // n must be a positive root number for subsystem
+    return sub_root[n].reflection; // parent reflection corresponding to |s|
+  }
 
   Coweight sub_2rho() const { return rd.dual_twoRho(pos_map); }
   Weight parent_sub_2rho() const { return rd.twoRho(pos_map); }

commit 5b0f72fa1028e93396cb3e10a691002f313cd122

Fix bug introduced in commit 970b4b8 (Jul 7 2015) in |Rep_context::any_Cayley|
This was due to confusion between full root number and positive root number
for the subsystem, and resulted in the failing of valid Cayley transforms

sources/gkmod/repr.cpp
@@ -571,13 +571,17 @@ StandardRepr Rep_context::any_Cayley(const Weight& alpha, StandardRepr z) const
     throw std::runtime_error("Not an integral root");
   // apply the integrally-dominant-making $W$ element |w| (in |subsys|) to |rt|:
   rt = subsys.permuted_root(rt,w); // now we've got the root to do Cayley by
-  rt = subsys.rt_abs(rt); // henceforth interpret as index of a positive root
+  const RootNbr n_alpha = subsys.to_parent(rt); // for modified |alpha|
+
+  rt = subsys.rt_abs(rt); // interpret as index of a positive root
+  weyl::Generator s=subsys.simple(rt);
+  WeylWord ww = subsys.to_simple(rt);
+  // neither |alpha| nor |rt| will be used beyond thus point
 
   // now do the Cayley transform proper
   bool ascent; // whether forward Cayley
-  InvolutionNbr inv0= kgb.inv_nr(x);
-  weyl::Generator s=subsys.simple(rt);
-  WeylWord ww = subsys.to_simple(rt);
+  const InvolutionNbr inv0= kgb.inv_nr(x); // initial involution
+
   x = kgb.cross(ww,x);
   switch (kgb.status(s,x))
   {
@@ -587,8 +591,8 @@ StandardRepr Rep_context::any_Cayley(const Weight& alpha, StandardRepr z) const
     { Weight rho2_diff = rd.twoRho() - rd.twoRho(i_tab.real_roots(inv0));
       RatWeight parity_vector = // compute this at the \emph{original} x
 	infin_char - lr - RatWeight(std::move(rho2_diff),2);
-      if (parity_vector.dot(rd.coroot(subsys.to_parent(rt)))%2!=0)
-      { // then |rt| was parity
+      if (parity_vector.dot(rd.coroot(n_alpha))%2!=0)
+      { // then |alpha| was parity
 	x = kgb.inverseCayley(s,x).first; // do inverse Cayley at |inv1|
 	ascent=false;
 	break;