8bbc10a11e0f38e38bb87d019750c2f9c636dffd
David Vogan <dav@math.mit.edu>
2020-07-03 10:30:09 -0400

    Added time info to printing in deformation

--- sources/gkmod/repr.cpp
+++ sources/gkmod/repr.cpp
@@ -1319,21 +1319,25 @@ SR_poly Rep_table::deformation_terms
     }
     assert(it==finals.end());
   }
-  unsigned resident;
+  unsigned resident, CPUtime;
+  std::string MemUnits = "MB";
+  std::string TimeUnits = " secs";
   struct rusage usage;
       if(getrusage(RUSAGE_SELF, &usage) != 0)
 	std::cerr << "getrusage failed" << std::endl;
       resident = usage.ru_maxrss/1024; //largest so far??
+      CPUtime = usage.ru_utime.tv_sec;
+      
 #ifdef __APPLE__
       resident = resident/1024;
-#endif  
-  std::cerr //  << "            #def_forms = " << def_formulae.size()
-	     << "            #alcove_def_forms = "
+#endif
+      if (resident > 10240) {MemUnits = "GB" ; resident=resident/1024;}
+      if (CPUtime > 599) {TimeUnits = " mins"; CPUtime = CPUtime/60;}
+  std::cerr //  << "             #def_forms = " << def_formulae.size()
+	     << "             #alcv_forms = "
 	     << alcove_def_formulae.size()
-	     << "  max res size = " << resident << "MB\r";
-    //	     << "  # trans fams = " << mod_pool.size() << "\r";
-  //	     << " # looked-up = " << LOOKUP 
-
+	     << " max res size = " << resident << MemUnits 
+  << " CPU time = " << CPUtime << TimeUnits << "\r";
 
   return result;
 } // |deformation_terms|, common block version
@@ -1676,18 +1680,24 @@ SR_poly Rep_table::twisted_deformation_terms
     assert(it==acc.end());
   }
   unsigned resident;
+  std::string MemUnits = "MB";
+  std::string TimeUnits = " secs";
   struct rusage usage;
       if(getrusage(RUSAGE_SELF, &usage) != 0)
 	std::cerr << "getrusage failed" << std::endl;
       resident = usage.ru_maxrss/1024; //largest so far??
+      CPUtime = usage.ru_utime.tv_sec;
 #ifdef __APPLE__
       resident = resident/1024;
 #endif
-      
-  std::cerr  //   << "              #def_forms = " << def_formulae.size()
-	     << "                #alcove_def_forms = "
+
+      if (resident > 10240) {MemUnits = "GB" ; resident=resident/1024;}
+      if (CPUtime > 599) {TimeUnits = " mins"; CPUtime = CPUtime/60;}
+  std::cerr //  << "             #def_forms = " << def_formulae.size()
+	     << "             #alcv_forms = "
 	     << alcove_def_formulae.size()
-	     << "  max res size = " << resident << "MB\r";
+	     << " max res size = " << resident << MemUnits
+  << " CPU time = " << CPUtime << TimeUnits << "\r";
 
   return result;
 } // |twisted_deformation_terms(blocks::common_block&,...)|
