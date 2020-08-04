jeffreyadams/atlasofliegroups

commit c9c49bdd4eda4c62bbe28ef740863bc6dc80a37a

Make builtin |W_graph@Param| use a |common_block| rather than a |param_block|

sources/interpreter/atlas-types.w
@@ -5082,13 +5082,13 @@ export that.
 
 @< Local function def...@>=
 void param_W_graph_wrapper(expression_base::level l)
-{ shared_module_parameter p = get<module_parameter_value>();
+{ own_module_parameter p = get_own<module_parameter_value>();
   test_standard(*p,"Cannot generate block");
   if (l==expression_base::no_value)
     return;
 @)
   BlockElt start; // will hold index in the block of the initial element
-  param_block block(p->rc(),p->val,start);
+  auto& block = p->rt().lookup_full_block(p->val,start);
   push_value(std::make_shared<int_value>(start));
 @)
   const kl::KL_table& kl_tab = block.kl_tab(block.size()-1,nullptr,false);
@@ -5104,13 +5104,14 @@ void param_W_graph_wrapper(expression_base::level l)
 }
 @)
 void param_W_cells_wrapper(expression_base::level l)
-{ shared_module_parameter p = get<module_parameter_value>();
+{ own_module_parameter p = get_own<module_parameter_value>();
   test_standard(*p,"Cannot generate block");
   if (l==expression_base::no_value)
     return;
 @)
   BlockElt start; // will hold index in the block of the initial element
-  param_block block(p->rc(),p->val,start);
+  auto& block = p->rt().lookup_full_block(p->val,start);
+  push_value(std::make_shared<int_value>(start));
 @)
   const kl::KL_table& kl_tab = block.kl_tab(block.size()-1,nullptr,false);
    // this does the actual KL computation

sources/gkmod/ext_block.cpp
@@ -222,11 +222,11 @@ unsigned int link_count(DescValue v)
 unsigned int scent_count(DescValue v)
 { return has_double_image(v) ? 2 : link_count(v)==0 ? 0 : 1; }
 
-// find element |n| such that |z(n)>=zz|
+// find |n| such that |z(n)>=zz| (for |zz==parent.size()| returns |n==size()|)
 BlockElt ext_block::element(BlockElt zz) const
 {
   BlockElt min=0, max=size();
-  // loop invariant: |(min==0 or z(min-1)<zz) and (max<size() or z(max)>=zz)|
+  // loop invariant: |(min==0 or z(min-1)<zz) and (max>=size() or z(max)>=zz)|
   while (max>min)
   {
     BlockElt x=(min+max)/2;
@@ -2223,9 +2223,6 @@ RankFlags reduce_to(const ext_gens& orbits, RankFlags gen_set)
   return result;
 }
 
-
-RankFlags ext_block::singular_orbits (const param_block& parent) const
-{ return reduce_to(orbits,parent.singular_simple_roots()); }
 
 weyl::Generator
 ext_block::first_descent_among(RankFlags singular_orbits, BlockElt y) const

sources/gkmod/ext_block.h
@@ -97,7 +97,12 @@ DescValue extended_type(const Block_base& block, BlockElt z, const ext_gen& p,
 			BlockElt& first_link);
 
 
-typedef Polynomial<int> Pol;
+ using Pol = Polynomial<int>;
+
+
+// flag those among |orbits| whose elements are flagged in |gen_set|
+// here |gen_set| is supposed a union of orbits, so (any flagged => all flagged)
+RankFlags reduce_to(const ext_gens& orbits, RankFlags gen_set);
 
 class ext_block
 {
@@ -198,9 +203,9 @@ class ext_block
   // whether link for |s| from |x| to |y| has a sign flip attached
   int epsilon(weyl::Generator s, BlockElt x, BlockElt y) const;
 
-  // this works only for blocks generated from parameters, so supply |parent|
-  // mark the extended generators (orbits) that are singular for |parent.gamma()|
-  RankFlags singular_orbits(const param_block& parent) const;
+  // transform |singular| to a bitset of |orbits|
+  RankFlags singular_orbits(RankFlags singular) const
+    { return reduce_to(orbits,singular); }
 
   weyl::Generator first_descent_among
     (RankFlags singular_orbits, BlockElt y) const;
@@ -229,10 +234,6 @@ private:
   bool tune_signs(const blocks::common_block& block);
 
 }; // |class ext_block|
-
-// flag those among |orbits| whose elements are flagged in |gen_set|
-// here |gen_set| is supposed a union of orbits, so (any flagged => all flagged)
-RankFlags reduce_to(const ext_gens& orbits, RankFlags gen_set);
 
 // Extended parameters
 

sources/gkmod/ext_kl.cpp
@@ -911,6 +911,7 @@ bool KL_table::check_polys(BlockElt y) const
   return result;
 }
 
+// this function serves uniquely to implement the built=in function of that name
 void ext_KL_matrix (const StandardRepr p, const int_Matrix& delta,
 		    const Rep_context& rc, // the rest is output
 		    std::vector<StandardRepr>& block_list,
@@ -922,8 +923,11 @@ void ext_KL_matrix (const StandardRepr p, const int_Matrix& delta,
     std::cout << "Delta does not fix gamma=" << p.gamma() << "." << std::endl;
     throw std::runtime_error("No valid extended bock");
   }
-  param_block B(rc,p,entry_element);
-  ext_block::ext_block eblock(B,delta);
+  auto srm = repr::StandardReprMod::mod_reduce(rc,p); // modular |z|
+  blocks::common_block B(rc,srm,entry_element);
+  const auto& gamma = p.gamma();
+  const RankFlags singular = B.singular(gamma);
+  ext_block::ext_block eblock(B,delta,nullptr);
 
   BlockElt size= // size of extended block we shall use; before compression
     eblock.element(entry_element+1);
@@ -961,7 +965,7 @@ void ext_KL_matrix (const StandardRepr p, const int_Matrix& delta,
 */
 
   containers::sl_list<BlockElt> survivors
-    (eblock.condense(P_mat,eblock.singular_orbits(B)));
+    (eblock.condense(P_mat,eblock.singular_orbits(singular)));
 
   if (survivors.size()<size) // if any non-survivors, we need to compress |P_mat|
   { size=survivors.size(); // henceforth this is our size
@@ -988,12 +992,11 @@ void ext_KL_matrix (const StandardRepr p, const int_Matrix& delta,
   block_list.clear(); block_list.reserve(size);
   lengths = int_Vector(0); lengths.reserve(size);
 
-  const auto gamma = B.gamma();
   assert(is_dominant_ratweight(rc.root_datum(),gamma)); // from |param_block|
   for (auto ez : survivors)
   {
     auto z = eblock.z(ez);
-    block_list.push_back(rc.sr_gamma(B.x(z),B.lambda_rho(z),gamma));
+    block_list.push_back(rc.sr(B.representative(z),gamma));
     lengths.push_back(B.length(z));
   }
 

commit c234e9fcd708ea384995443574ad059009537cce

Fix bug introduced in c9c49bdd: spurious |push_value| in |param_W_cells_wrapper|
Pushing the same value twice caused any call to |W_cells@Param| to leave a value
on the runtime stack that in certian situations could cause an "Internal error".

sources/interpreter/atlas-types.w
@@ -5083,7 +5083,6 @@ void param_W_cells_wrapper(expression_base::level l)
   wgraph::WGraph wg = kl::wGraph(kl_tab);
   wgraph::DecomposedWGraph dg(wg);
 @)
-  push_value(std::make_shared<int_value>(start));
   own_row cells=std::make_shared<row_value>(0);
   cells->val.reserve(dg.cellCount());
   for (unsigned int c = 0; c < dg.cellCount(); ++c)