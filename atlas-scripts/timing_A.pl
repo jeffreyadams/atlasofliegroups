8c59ece3295df2e06fbcb81b98fa8972e4a3148e
Jeffrey Adams <jeffreydavidadams@gmail.com>
2020-09-02 13:59:30 -0400

    Modified timing_A.pl

--- atlas-scripts/timing_A.pl
+++ atlas-scripts/timing_A.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl
 #perl script for timing atlas calculations
 #calls atlas_timing.pl
-$label="A";
+$label="timing_A";
 
-`./atlas_timing.pl "SL(2,R)" >  timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(3,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(4,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(5,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(6,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(7,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(8,R)" >> timing_A.orig.txt 2>&1`;
-`./atlas_timing.pl "SL(9,R)" >> timing_A.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(2,R)" >  $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(3,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(4,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(5,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(6,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(7,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(8,R)" >> $label.orig.txt 2>&1`;
+`./atlas_timing.pl "SL(9,R)" >> $label.orig.txt 2>&1`;
 
 `grep -v "Cannot\\|double\\|Abandon\\|Command" $label.orig.txt > $label.txt`
 
