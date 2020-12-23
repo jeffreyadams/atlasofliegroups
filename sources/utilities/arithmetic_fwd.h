f8404845ce5d00ae3e9abb9e4ae9e71da8dfc920
Marc van Leeuwen <Marc.van-Leeuwen@math.univ-poitiers.fr>
2020-12-23 07:51:20 +0100

    Change the size of integers in rationals and ratvecs from |long long| to |int|

--- sources/utilities/arithmetic_fwd.h
+++ sources/utilities/arithmetic_fwd.h
@@ -18,8 +18,8 @@ namespace atlas {
 
 namespace arithmetic {
 
-  using Numer_t = long long int;
-  using Denom_t = unsigned long long int;
+  using Numer_t = int;
+  using Denom_t = unsigned int;
   template<typename I> class Rational;
   using RatNum = Rational<Numer_t>;
   using RatNumList = std::vector<RatNum>;
