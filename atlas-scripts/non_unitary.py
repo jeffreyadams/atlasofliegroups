f516f62197471c2e67efbf1dcbf149fd79789ad9
Jeffrey <jeffreydavidadams@gmail.com>
2023-09-13 09:12:44 -0400

    Intermediate commit, working on non_unitary.py

--- atlas-scripts/non_unitary.py
+++ atlas-scripts/non_unitary.py
@@ -21,49 +21,73 @@ def atlas_compute(i):
    print("starting atlas_compute process #", i)
    proc=procs[i]
 
-   #first read in file, get N=total number of parameters
-   atlas_arg='{}'.format("< " + input_file+ "\n").encode('utf-8')
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   while True:   #read until line with "Variable"
-      line = proc.stdout.readline().decode('ascii')
-      if "Variable" in line:
-         break
-      f.write(line)
-   print("Done reading in file " + str(input_file) + "(job #" + str(i) + ")")
-   
    output_file=directory + "/" + str(i)
    f=open(output_file,"w", buffering=1)
-   f.write("Starting computation at " + str(time.ctime()) + "\n")
-   f.write("Computing FPP_unitary_hash_one_level(G,hash) for G=" + group)
-   line= "Job number: "  + str(i) + "\n" + "Number of jobs: " + str(number_jobs) + "\n" + "Number of parameters: " + str(number_parameters) + "\n" + "Number of parameters this job: " + str(N) + "\n"
-   f.write(line)
-   starttime=time.time()
-   print("Reading in file  (job #" + str(i) + "): " + str(input_file))
-   #get number of parameters in file
+   f.write("{Starting computation at " + str(time.ctime()) + "\n")
+   f.write("Computing is_unitary_to_level_one(G,[Param])  for G=" + group + "\n")
+
+    #first read in file, get N=total number of parameters
+   atlas_arg='{}'.format("< " + input_file+ "\n").encode('utf-8')
+#   print("atlas_arg 1: ", atlas_arg)
+   proc.stdin.write(atlas_arg)
+   proc.stdin.flush()
+   count=0
+   while True:   #read until line with "Completely"
+      line = proc.stdout.readline().decode('ascii')
+      if "Completely" in line:
+         break
+#     f.write(line)
+#      print(line)
+      count += 1
+      if count%1000==0:
+         f.write(str(count) + "  " + line)
+   print("Done reading in file " + str(input_file) + "(job #" + str(i) + ")")
    atlas_arg='{}'.format("#parameters" + "\n").encode('utf-8')
+#   print("atlas_arg 2: ", atlas_arg)
    proc.stdin.write(atlas_arg)
    proc.stdin.flush()
    line = proc.stdout.readline().decode('ascii')
-   N=int(line)
-   print("N=",N)
-   f.write("Number of parameters; " + str(N))
-   atlas_arg ='{}'.format("set x=is_unitary_to_level_one(parameters,"  + str(i*N) + "," + "N" + ")" + "\n").encode('utf-8')
-   print("Atlas arg: ", atlas_arg)
-   
+   N=int(re.sub('\D','',line))
+   print("Total number of parameters: ", N)
+   n=N//number_jobs
+   f.write("Total number of parameters: " + str(N) + "\n")
+   f.write("Number of parameters per job: " + str(n) + "\n")
+   start_index=i*n
+   number_parameters_this_job=min(n,N-(i)*n)
+   end_index=start_index+number_parameters_this_job-1
+   print("Number of parameters this job: ", number_parameters_this_job)
+   f.write("Number of parameters this job: " + str(number_parameters_this_job) + "\n")
+   f.write("Starting index: " +  str(start_index) + "\n")
+   f.write("Ending index: " + str(end_index) + "\n")
+   line= "Job number: "  + str(i)  + "\n" + "Number of jobs: " + str(number_jobs) + "\n" + "Number of parameters: " + str(N) + "\n" + "Number of parameters this job: " + str(number_parameters_this_job) + "\n" + "Starting index: " + str(start_index) + "\n" + "Ending index: " + str(end_index) + "\n"
+   print("line=", line)
+   f.write(line + "}\n")
+   f.write("passed_" + str(i) + "=[\n")
+   starttime=time.time()
+   atlas_arg_0='{}'.format("set comma=\"\"" + "\n").encode('utf-8')
+   proc.stdin.write(atlas_arg_0)
+   proc.stdin.flush()
+   line = proc.stdout.readline().decode('ascii')  #get rid of one line
+   atlas_arg_1="for j:" + str(number_parameters_this_job) + " from " + str(start_index)
+   atlas_arg_2=" do let test=is_unitary_to_level_one(parameters[j]) in "
+   atlas_arg_3=  "if  true then prints(comma + \"(\"+ j+ \",\"+ test.to_string + \")\");comma:=\",\" fi od" + "\n"
+   atlas_arg=    atlas_arg_1 + atlas_arg_2 + atlas_arg_3 + "\n"
+   atlas_arg='{}'.format(atlas_arg).encode('utf-8')
+ #  print(atlas_arg)
    proc.stdin.write(atlas_arg)
    proc.stdin.flush()
-
-   while True:   #read until line with "Variable"
+   while True:   #read until line with "Value"
       line = proc.stdout.readline().decode('ascii')
-      if "Variable" in line:
+#      print("MY LINE: ", line)
+      if "Value" in line:
          break
       line = line.strip() + "  " +  time.strftime("%H:%M:%S", time.gmtime(time.time()-starttime)) + "\n"
       f.write(line)
-   print("Done computing nonunitary of parameters (job #" + str(i) + ")")
-
+#      print("line=", line)
+#   print("NOW line: ", line)
+   f.write("]\n")
    stoptime=time.time()
-   f.write("Finished computation at " + str(time.ctime()) + "\n")
+   f.write("{Finished computation at " + str(time.ctime()) + "}\n")
    elapsed = time.strftime("%H:%M:%S", time.gmtime(stoptime-starttime))
    print("Finished ",group, "  time: ", elapsed)
    f.write("elapsed time: "+ elapsed)
@@ -107,6 +131,7 @@ def main(argv):
          proc=procs[i]
          Q=P.submit(atlas_compute,i)
          T.append(Q)
+   exit()
 #   for t in T:
 #      try:
 #         data = t.result()
