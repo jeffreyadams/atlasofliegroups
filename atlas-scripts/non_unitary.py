01ae98eabcd668a42effffe11e6956a88a2b4026
Jeffrey <jeffreydavidadams@gmail.com>
2023-09-25 17:36:24 -0400

    updated non_unitary.py

--- atlas-scripts/non_unitary.py
+++ atlas-scripts/non_unitary.py
@@ -3,7 +3,7 @@
 #test file of ostensibly non-unitary representations
 #mainly intended for checking 67M non-unitary principal series for E8 calculated by Steve Miller
 
-import sys, time, os, getopt, subprocess, gc,re
+import sys, time, datetime, os, getopt, subprocess, gc,re
 import concurrent.futures
 from subprocess import Popen, PIPE, STDOUT
 import multiprocessing as mp   #only for cpu count
@@ -24,68 +24,73 @@ def atlas_compute(i):
    output_file=directory + "/" + str(i)
    f=open(output_file,"w", buffering=1)
    f.write("{Starting computation at " + str(time.ctime()) + "\n")
-   f.write("Computing is_unitary_to_level_one(G,[Param])  for G=" + group + "\n")
+   f.write("Computing is_unitary_to_level(" + level + ",G,p)  for G=" + group + "\n")
+   f.write("Level <0 <-> is_unitary(p) \n")
 
-    #first read in file, get N=total number of parameters
-   atlas_arg='{}'.format("< " + input_file+ "\n").encode('utf-8')
-#   print("atlas_arg 1: ", atlas_arg)
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   count=0
-   while True:   #read until line with "Completely"
-      line = proc.stdout.readline().decode('ascii')
-      if "Completely" in line:
-         break
-#     f.write(line)
-#      print(line)
-      count += 1
-      if count%1000==0:
-         f.write(str(count) + "  " + line)
-   print("Done reading in file " + str(input_file) + "(job #" + str(i) + ")")
-   atlas_arg='{}'.format("#parameters" + "\n").encode('utf-8')
-#   print("atlas_arg 2: ", atlas_arg)
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   line = proc.stdout.readline().decode('ascii')
-   N=int(re.sub('\D','',line))
-   print("Total number of parameters: ", N)
-   n=N//number_jobs
-   f.write("Total number of parameters: " + str(N) + "\n")
-   f.write("Number of parameters per job: " + str(n) + "\n")
-   start_index=i*n
-   number_parameters_this_job=min(n,N-(i)*n)
-   end_index=start_index+number_parameters_this_job-1
-   print("Number of parameters this job: ", number_parameters_this_job)
+   #get N=total number of parameters from number of lines in input file
+   N=int(re.search(r'\d+',(os.popen("wc -l " + input_file).read())).group())
+   print("Number of all of parameters: ", N)
+   #set n=number_jobs
+   #define m,r: N=nm + r   (r<n)
+   #number of jobs each process:  (m+1,m+1,...,m+1,m,...,m)  (r ones)
+   #j   start_index(j)   end_index(j)
+   #0   0                m
+   #1   m+1              2m+1
+   #j   j(m+1)           (j+1)m+j   (j\le r-1)
+   #r-1 (r-1)(m+1)       rm+r-1    so far: rm+r-1+1=rm+r parameters
+   #r   rm+r             (r+1)m+(r-1)
+   #r+1 (r+1)m+r         (r+2)m+(r-1)
+   #n-1 (n-1)m+r         nm+r-1     total: nm+r=N
+   #formula:
+   #start_index(i)=im+min(i,r)
+   #end_index(i)  =(i+1)m+min(i,r-1)
+   n=number_jobs
+   m=N//n  #all jobs approximately same size
+   r=N-n*m
+   start_index=i*m + min(i,r)
+   end_index=(i+1)*m + min(i,r-1)
+   print(i, " ", start_index, " ", end_index)
+   f.write("Number of all parameters: " + str(N) + "\n")
+   f.write("Number of parameters per job: " + str(m) + "/" + str(m+1)  + "\n")
+   number_parameters_this_job=end_index-start_index+1
+
    f.write("Number of parameters this job: " + str(number_parameters_this_job) + "\n")
    f.write("Starting index: " +  str(start_index) + "\n")
-   f.write("Ending index: " + str(end_index) + "\n")
-   line= "Job number: "  + str(i)  + "\n" + "Number of jobs: " + str(number_jobs) + "\n" + "Number of parameters: " + str(N) + "\n" + "Number of parameters this job: " + str(number_parameters_this_job) + "\n" + "Starting index: " + str(start_index) + "\n" + "Ending index: " + str(end_index) + "\n"
-   print("line=", line)
-   f.write(line + "}\n")
+   f.write("Ending index: " + str(end_index) + "\n}\n")
    f.write("passed_" + str(i) + "=[\n")
    starttime=time.time()
+   #some initialization
+   atlas_arg='{}'.format("set G=" + group + "\n").encode('utf-8')
+#   print("atlas_arg: ", atlas_arg)
+   proc.stdin.write(atlas_arg)
+   proc.stdin.flush()
+   line = proc.stdout.readline().decode('ascii')  #discard this line
    atlas_arg_0='{}'.format("set comma=\"\"" + "\n").encode('utf-8')
    proc.stdin.write(atlas_arg_0)
    proc.stdin.flush()
-   line = proc.stdout.readline().decode('ascii')  #get rid of one line
-   atlas_arg_1="for j:" + str(number_parameters_this_job) + " from " + str(start_index)
-   atlas_arg_2=" do let test=is_unitary_to_level_one(parameters[j]) in "
-   atlas_arg_3=  "if  true then prints(comma + \"(\"+ j+ \",\"+ test.to_string + \")\");comma:=\",\" fi od" + "\n"
-   atlas_arg=    atlas_arg_1 + atlas_arg_2 + atlas_arg_3 + "\n"
-   atlas_arg='{}'.format(atlas_arg).encode('utf-8')
- #  print(atlas_arg)
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   while True:   #read until line with "Value"
-      line = proc.stdout.readline().decode('ascii')
-#      print("MY LINE: ", line)
-      if "Value" in line:
-         break
-      line = line.strip() + "  " +  time.strftime("%H:%M:%S", time.gmtime(time.time()-starttime)) + "\n"
+   line = proc.stdout.readline().decode('ascii')  #discard this line
+#   print("discard: ", line)
+   sed_arg="sed -n \'" + str(start_index+1) + "," + str(end_index+1) + " p\' " + input_file
+#   print("sed_arg: ", sed_arg)
+   parameters_from_file =os.popen(sed_arg).read()
+   parameters=parameters_from_file.splitlines()
+   comma=""
+   for j in range(len(parameters)):
+      atlas_arg='{}'.format("let test=is_unitary_to_level(" + level + "," + parameters[j] + ") in prints(test, \" \", deformLookupCounter.use_count(), \" \", deformCalcCounter.use_count()) \n").encode('utf-8')
+#      atlas_arg='{}'.format("let test=is_unitary_to_level(" + level + "," + parameters[j] + ") in prints(test) \n").encode('utf-8')
+      print("atlas_arg: ", atlas_arg)
+      proc.stdin.write(atlas_arg)
+      proc.stdin.flush()
+
+      line = proc.stdout.readline().decode('ascii').strip()
+      print("line: ", line)
+      line = comma + "(" + str(start_index + j) + "," + parameters[j] + "," + line + ")"
+#      line = line + "  {" +  time.strftime("%H:%M:%S", time.gmtime(time.time()-starttime)) + "}\n"
+      line = line + "  {"  +str(datetime.timedelta(seconds=384374974+time.time()-starttime)) + "}\n"
       f.write(line)
-#      print("line=", line)
-#   print("NOW line: ", line)
+      comma=","
    f.write("]\n")
+
    stoptime=time.time()
    f.write("{Finished computation at " + str(time.ctime()) + "}\n")
    elapsed = time.strftime("%H:%M:%S", time.gmtime(stoptime-starttime))
@@ -95,11 +100,11 @@ def atlas_compute(i):
    return()
 
 def main(argv):
-   global directory, number_jobs,input_file,group
+   global directory, number_jobs,input_file,group, level
    group="E8_s"
-   opts, args = getopt.getopt(argv, "d:i:n:")
+   opts, args = getopt.getopt(argv, "d:i:n:g:l:")
    if len(opts)<3:
-      print("Usage: \n-d: directory\n-i: input_file\n-n: number jobs")
+      print("Usage: \n-d: directory\n-i: input_file\n-n: number jobs\n-g: group\n-l: level  (level <0 <-> is_unitary(p))")
       exit()
    print("----------------------------")
    print("Starting at ", time.ctime())
@@ -112,6 +117,10 @@ def main(argv):
           print("directory: ", directory)
           if not os.path.exists(directory):
              os.makedirs(directory)
+       elif opt in ('-g'):
+          group=arg
+       elif opt in ('-l'):
+          level=arg
        elif opt in ('-i'):
           input_file=arg
           print("input_file: ", input_file)
