cfc76db234f8f19a4169a65644f22a4089c12feb
Jeffrey <jeffreydavidadams@gmail.com>
2023-10-16 09:50:44 -0400

    major changes to FPP.py

--- atlas-scripts/FPP.py
+++ atlas-scripts/FPP.py
@@ -17,50 +17,16 @@ def report(q,i,proc):
 #procs: array of processes
 #i: number of process
 def atlas_compute(i,pid):
-   print("starting atlas_compute process #", i)
+   print("starting atlas_compute process #", i, "pid: ", str(pid))
    proc=procs[i]
+
    data_file=directory + "/" + str(i) + ".at"
    log_file=directory + "/logs/" + str(i)
    data=open(data_file,"w", buffering=1)
    log=open(log_file,"w", buffering=1)
    log.write("{Starting computation at " + str(time.ctime()) + "\n")
    log.write("Computing FPP for " + group + "\n")
-   log.write("Process number: " + str(i))
-   log.write("Process id: " + str(pid))
-   atlas_arg='{}'.format("KGB_size(" + group + ")\n").encode('utf-8')
-#   print("atlas_arg: ", atlas_arg)
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   N=int(re.sub(".* ","",proc.stdout.readline().decode('ascii')))
-#   print("N", str(N))
-   
-   #set n=number_jobs
-   #define m,r: N=nm + r   (r<n)
-   #number of jobs each process:  (m+1,m+1,...,m+1,m,...,m)  (r ones)
-   #j   start_index(j)   end_index(j)
-   #0   0                m
-   #1   m+1              2m+1
-   #j   j(m+1)           (j+1)m+j   (j\le r-1)
-   #r-1 (r-1)(m+1)       rm+r-1    so far: rm+r-1+1=rm+r parameters
-   #r   rm+r             (r+1)m+(r-1)
-   #r+1 (r+1)m+r         (r+2)m+(r-1)
-   #n-1 (n-1)m+r         nm+r-1     total: nm+r=N
-   #formula:
-   #start_index(i)=im+min(i,r)
-   #end_index(i)  =(i+1)m+min(i,r-1)
-   n=number_jobs
-   m=N//n  #all jobs approximately same size
-   r=N-n*m
-#   print("r=",r,"  m=", m, "i= ", i)
-   start_index=int(start_KGB)+i*m + min(i,r)
-   end_index=int(start_KGB)+(i+1)*m + min(i,r-1)
-#   print(i, " ", start_index, " ", end_index)
-   log.write("Number of KGB elements: " + str(N) + "\n")
-   log.write("Number of KGB elements per job: " + str(m) + "/" + str(m+1)  + "\n")
-   number_kgb_elements_this_job=int(end_index)-int(start_index)+1
-   log.write("Number of KGB Elements this job: " + str(number_kgb_elements_this_job) + "\n")
-   log.write("Starting index: " +  str(start_index) + "\n")
-   log.write("Ending index: " + str(end_index) + "\n}\n")
+   log.write("Job number: " + str(i))
 
    starttime=time.time()
    #some initialization
@@ -70,14 +36,30 @@ def atlas_compute(i,pid):
    proc.stdin.write(atlas_arg)
    proc.stdin.flush()
    line = proc.stdout.readline().decode('ascii')  #discard this line
-#   print("LINE: ", line)
-   
-#   print("discard: ", line)
-   atlas_arg='{}'.format("set list=FPP(" + group + "," + str(start_index) + "," + str(number_kgb_elements_this_job) + ")"+ "\n").encode('utf-8')
-#   print("atlas_arg: ", atlas_arg)
-   proc.stdin.write(atlas_arg)
-   proc.stdin.flush()
-   newtime=starttime
+   time.sleep(1)
+   while True:
+      print("TRUE")
+      head_arg="head -1 " + input_file 
+      print("head_arg:", head_arg)
+      kgb_line=os.popen(head_arg).read()
+      print("kgb_line: ", kgb_line + "pid=" + str(pid))
+      if len(kgb_line)==0:
+         print("no more kgb elements")
+         break
+      sed_arg="sed  1d -i " + input_file
+      print("sed_arg: ", sed_arg)
+      delete_line=os.popen(sed_arg)
+      print("lines:")
+#      atlas_arg='{}'.format("set list=FPP(" + group + "," + str(KGB_number) + ")" + "\n").encode('uft-8')
+      aa="set list=FPP(" + str(group) +  str(KGB_number)
+      print("aa=",aa)
+      atlas_arg='{}'.format(aa).encode('uft-8')
+      print("OKOKOK")
+      print("atlas_arg: ", atlas_arg)
+      proc.stdin.write(atlas_arg)
+      proc.stdin.flush()
+      newtime=starttime
+   print("NOW")
    while True:
       oldtime=newtime
       newtime=time.time()
@@ -111,11 +93,12 @@ def atlas_compute(i,pid):
    return()
 
 def main(argv):
-   global directory, number_jobs,group, start_KGB
+   global directory, number_jobs,group, start_KGB,end_KGB,input_file
    group="E8_s"
    start_KGB=0
+   input_file=""
    end_KGB=1000  #should probably be KGB_size(G)
-   opts, args = getopt.getopt(argv, "d:n:g:k:i:")
+   opts, args = getopt.getopt(argv, "d:n:g:k:e:i:")
    if len(opts)<4:
       print("Usage: \n-d: directory\n-n: number jobs\n-g: group\n-k: start KGB\n")
       exit()
@@ -133,9 +116,14 @@ def main(argv):
              os.makedirs(directory + "/logs")
        elif opt in ('-g'):
           group=arg
+       elif opt in ('-i'):
+          input_file=arg
        elif opt in ('-k'):
           start_KGB=arg
           print("starting KGB: ", start_KGB)
+       elif opt in ('-e'):
+          end_KGB=arg
+          print("ending KGB: ", end_KGB)
        elif opt in ('-n'):
           number_jobs=int(arg)
           print("number_jobs: ", number_jobs)
@@ -146,7 +134,8 @@ def main(argv):
    for i in range(number_jobs):
       proc=subprocess.Popen(["../atlas","FPP.at"], stdin=PIPE,stdout=PIPE)
       procs.append(proc)
-      print("pid: ", proc.pid)
+      pid=proc.pid
+      print("pid: ", pid)
 #   print("number of processes: ", len(procs))
    with concurrent.futures.ProcessPoolExecutor(number_jobs) as P:
       for i in range(number_jobs):
