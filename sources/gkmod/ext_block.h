5c931e539beac452de36bc4215d16da2926455f0
Marc van Leeuwen <Marc.van-Leeuwen@math.univ-poitiers.fr>
2024-06-24 11:56:29 +0200

    Improve comments to better understand simply integral flagging in modified blocks

--- sources/gkmod/ext_block.h
+++ sources/gkmod/ext_block.h
@@ -155,8 +155,7 @@ class ext_block
 // manipulators
   void flip_edge(weyl::Generator s, BlockElt x, BlockElt y);
 
-  const ext_kl::KL_table& kl_table
-    (BlockElt limit, ext_KL_hash_Table* pool=nullptr);
+  const ext_kl::KL_table& kl_table(ext_KL_hash_Table* pool,BlockElt limit);
 
   void swallow // integrate an older partial block, with mapping of elements
   (ext_block&& sub, const BlockEltList& embed, const Permutation& simple_pi);
