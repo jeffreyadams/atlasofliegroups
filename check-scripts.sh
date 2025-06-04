e0de9f5e11ff539d3d556370b967608f346cbd15
Marc van Leeuwen <Marc.van-Leeuwen@math.univ-poitiers.fr>
2025-06-04 05:53:21 +0200

    Fix several small issues that made scripts fail to load
    Mainly this is about |Maybe<T>| types (as produced by verions of |solve|),
    initializing variables to [] (which must now be cast, lest the variable be
    implicitly declared constant so that assignments fail) and a few name changes
    (solution->some, no_mat->none, update_matrix_entry->matrix_assign, and
    e->identity_row). For the now missing |in_list| a quick local definition was
    given where needed. And on passing trailing whitespace was zapped.

--- check-scripts.sh
+++ check-scripts.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 #.ax files are not checked, use this for large files, for example bigblocke8.ax
 
 cd atlas-scripts >/dev/null
 for i in $(git ls-files | grep '\.at$')
 do
-echo $i;   #uncomment this to get a running report on files checked
+echo -ne $i '\r';   #uncomment this to get a running report on files checked
 if ! ../atlas <$i >/dev/null 2>/dev/null; then echo Problem loading $i; fi
 done
-echo Done
+echo -e '\n' Done
