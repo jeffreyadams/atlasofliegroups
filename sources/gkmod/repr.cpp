5376acbcdd04d3e96137b628e3410b48ab6a06c1
Marc van Leeuwen <Marc.van-Leeuwen@math.univ-poitiers.fr>
2025-12-08 00:27:28 +0100

    Fix error in logic of hashing deformation unit in case of twisted deformation
    When a previously computed twisted deformation is used, the test for whether a
    flip is needed with respect to the default extension used when it was stored
    requires the stored sample to be delta-fixed (and correspond to the twisted
    terms stored). In order for this to work, the stored |sample| parameter needs to
    be updated when a twisted deformation is first requested in an alcove that
    already has been visted for an untwisted deformation. That update is now done.

--- sources/gkmod/repr.cpp
+++ sources/gkmod/repr.cpp
@@ -2312,7 +2312,7 @@ const deformation_unit& Rep_table::deformation(const StandardRepr& z)
 {
   assert(is_final(z));
 
-  auto h=alcove_hash.match(deformation_unit(*this,z));
+  auto h=alcove_hash.match(deformation_unit(*this,z,false));
   {
     deformation_unit& result = pool[h];
     if (result.has_def_contrib())
@@ -2632,7 +2632,9 @@ const deformation_unit&
   assert(is_delta_fixed(z));
   const auto& delta = inner_class().distinguished();
 
-  auto h = alcove_hash.match(deformation_unit(*this,z));
+  auto h = alcove_hash.match(deformation_unit(*this,z,true));
+  if (not pool[h].is_delta_fixed()) // previous value was from untwisted case
+    pool[h].replace_sample(z); // replace by our value, was tested delta-fixed
   if (pool[h].has_twdef_contrib())
   {
     Ext_rep_context ctxt(*this,delta);
